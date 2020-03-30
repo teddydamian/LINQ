53aa96d28eff22ba9fd4c97316677c6515176b8e
teddydamian <teddy.damian@gmail.com>
2020-03-30 13:08:32 -0700

    all pics

--- LINQ/LINQ/Program.cs
+++ LINQ/LINQ/Program.cs
@@ -37,41 +37,90 @@ namespace LINQ
             }
         }
 
+        /// <summary>
+        /// Query for all calls
+        /// </summary>
+        /// <param name="rootObject">List after parse</param>
         static void Query(RootObject rootObject)
         {
-            
-            var queryOneList = rootObject.features.Select(x => x.properties.Neighborhood);           
-            Console.WriteLine("Show all Neighborhoods: ");
+            // Query 1 : Lambda for all list including no neighbordhood
+            var allList = rootObject.features.Select(x => x.properties.Neighborhood);           
+            Console.WriteLine("Press Enter to show all Neighborhoods.");
+            Console.ReadLine();
 
             int count = 0;
             
-                foreach (string neighborhood in queryOneList)
+                foreach (string neighborhood in allList)
                 {
 
-                if (neighborhood.Equals(""))
-                    {
-                    count++;
-                        Console.WriteLine($"{ count}. No hood");
-                    }
-                    else
-                    {
                         count++;
-                        Console.Write($"{count}. {neighborhood} \n");
-                    }
+                        Console.Write($" {count}. {neighborhood} ;");
+                    
                 }
-            
+
             Console.WriteLine();
-            Console.Write("Press Enter to see 2");
+            // Query 2 : Lambda for alllist without no hood
+            Console.WriteLine();
+            Console.Write("Press Enter to see to see everything excluding ones with no hoods.");
             Console.ReadLine();
 
-            var noDuplicates = queryOneList.Where(x => x != "");
-            var queryTwoList = noDuplicates.Distinct();
+            var listWithoutNoHood = allList.Where(x => x != "");
             count = 0;
-            foreach (string hood in queryTwoList)
+            foreach (string neighborhood in listWithoutNoHood)
+            {
+
+                if (neighborhood.Equals(""))
+                {
+                    count++;
+                    Console.WriteLine($"{ count}. No hood");
+                }
+                else
+                {
+                    count++;
+                    Console.Write($" {count}. {neighborhood} ;");
+                }
+            }
+            Console.WriteLine();
+            // Query 3 : Distinct(); to show no duplicates
+            Console.WriteLine();
+            Console.Write("Press Enter to see to see everything without duplicates. \n");
+            Console.ReadLine();
+
+            var noDuplicates = listWithoutNoHood.Distinct();
+            count = 0;
+            foreach (string hood in noDuplicates)
             {
                     count++;
-                    Console.WriteLine($"{count}. {hood} ");
+                    Console.WriteLine($" {count}. {hood}");
             }
+
+            // Query 4 : Lambda with 1, 2, 3
+            Console.WriteLine();
+            Console.Write("Press Enter to see to see everything using single query.");
+            Console.ReadLine();
+            var allInOne = allList.Where(x => x != "").Distinct();
+            count = 0;
+            foreach (string hood in allInOne)
+            {
+                count++;
+                Console.WriteLine($" {count}. {hood}");
+            }
+
+            // Query 5 : Combining 1, 2, 3
+            Console.WriteLine();
+            Console.Write("Press Enter to see to see everything using LINQ query.");
+            Console.ReadLine();
+
+            var linqQuery = (from item in rootObject.features where item.properties.Neighborhood != "" select item.properties.Neighborhood).Distinct();
+
+            count = 0;
+            foreach (string hood in linqQuery)
+            {
+                count++;
+                Console.WriteLine($" {count}. {hood}" +
+                    $"");
+            }
+
         }
     }
 }
